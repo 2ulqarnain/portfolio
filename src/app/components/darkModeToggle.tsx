6f7754ff51175a62fd743c87d4b27fccebc811ee
2ulqarnain <zulqernaynh@gmail.com>
2024-09-13 01:01:37 +0500

    add: dark mode

---
+++ src/app/components/darkModeToggle.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { useState } from "react";
+
+export default function DarkModeToggle(){
+
+    const [isDarkMode, setIsDarkMode] = useState(false)
+
+    const handleDarkMode = () => {
+      document.documentElement.classList.toggle("dark")
+      setIsDarkMode(!isDarkMode)
+    }
+    
+    return (
+      <button className={`absolute top-16 right-16 flex items-center gap-2 dark:bg-zinc-800 transition-[background-color,color,border] duration-500 border border-zinc-400 dark:border-zinc-700 p-2 px-3 rounded-lg dark:text-orange-400 text-electric`} onClick={handleDarkMode}>
+        {
+          isDarkMode ? (
+            <><code>Light</code> <SunIcon /></>
+          ) : (
+            <><code>Dark</code> <MoonIcon /></>
+          )
+        }
+      </button>
+    )
+  }
+
+
+function SunIcon(){
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+    <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
+  </svg>
+  
+    )
+  }
+  
+  function MoonIcon(){
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+        <path fillRule="evenodd" d="M9.528 1.715a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z" clipRule="evenodd" />
+      </svg>
+    )
+  }
